55b4a6cd9aa3bbfc865f31adc7257bba90545e4a
PMPC025 <e.ryzhov@paymantix.com>
2015-06-02 20:00:08 +0300

    Time to go home

--- markgen.py
+++ markgen.py
@@ -1,1 +1,11 @@
-print "HW!"
+
+def download_files(source):
+	filenames=[]
+	return filenames
+
+def add_file_to_chains(files):
+	pass
+
+def learn(files):
+	for file in files:
+		add_file_to_chains(file)
